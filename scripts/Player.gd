b735eb9da28ad57ef4dc58dacaaabe8bfd2d90e3
Enzo Cranford <enzo@cranford.fr>
2021-06-01 19:41:23 +0200

    Add player boost

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,15 +1,27 @@
 extends KinematicBody2D
 
 var speed : int = 350
+var pull : int = 10
+var boost_strength : int = 100
+
+var is_boosting : bool = false
 var direction : Vector2 = Vector2.ZERO
-var pull : int = 5
+
+onready var boost_timer : Timer = get_node("Boost Timer")
 
 func _ready():
 	set_process(true)
 
+func _input(event):
+	if event.is_action_pressed("boost"):
+		boost()
+
 func _process(delta):
 	direction = Vector2.ZERO
-	direction.y += pull * delta
+	if is_boosting == true:
+		direction.y -= boost_strength * delta
+	else:
+		direction.y += pull * delta
 
 	if Input.is_action_pressed("move_right"):
 		direction.x += speed * delta
@@ -17,3 +29,9 @@ func _process(delta):
 		direction.x -= speed * delta
 
 	move_and_collide(direction)
+
+func boost():
+	boost_timer.start()
+	is_boosting = true
+	yield(boost_timer, "timeout")
+	is_boosting = false
