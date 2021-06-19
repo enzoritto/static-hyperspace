b949b3875cb055a50fe28ad38761f8fdb8c0f9f8
Enzo Cranford <enzo@cranford.fr>
2021-06-19 20:38:08 +0200

    Improve player movement
    Switch move_and_collide with move_and_slide & _process with _physics_process

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -13,13 +13,13 @@ onready var boost_timer : Timer = get_node("Boost Timer")
 onready var boost_bar : ProgressBar = get_node("../GUI/Boost Bar")
 
 func _ready():
-	set_process(true)
+	set_physics_process(true)
 
 func _input(event):
 	if event.is_action_pressed("boost") and boost_ammount >= max_boost_ammount:
 		boost()
 
-func _process(delta):
+func _physics_process(delta):
 	boost_bar.value = boost_ammount
 	
 	if not boost_ammount >= max_boost_ammount:
@@ -27,16 +27,16 @@ func _process(delta):
 	
 	direction = Vector2.ZERO
 	if is_boosting == true:
-		direction.y -= boost_strength * delta
+		direction.y -= boost_strength
 	else:
-		direction.y += pull * delta
+		direction.y += pull
 
 	if Input.is_action_pressed("move_right"):
-		direction.x += speed * delta
-	elif Input.is_action_pressed("move_left"):
-		direction.x -= speed * delta
+		direction.x += speed
+	if Input.is_action_pressed("move_left"):
+		direction.x -= speed
 
-	move_and_collide(direction)
+	move_and_slide(direction, Vector2.UP)
 
 func boost():
 	boost_timer.start()
